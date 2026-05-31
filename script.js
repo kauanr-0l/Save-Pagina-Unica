ee263376b6741bdb19230907ddffdbc71a7ea58b
kauanr <kauanrodrigues63624@gmail.com>
2026-05-31 07:00:09 -0300

    colocando as informação

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const 
+const btnDesafio1 = document.querySelector("btnDesafio1")
+const btnDesafio2 = document.querySelector("btnDesafio2")
+const btnDesafio3 = document.querySelector("btnDesafio3")
+const btnDesafio4 = document.querySelector("btnDesafio4")
+const btnDesafio5 = document.querySelector("btnDesafio5")
+const btnDesafio6 = document.querySelector("btnDesafio6")
