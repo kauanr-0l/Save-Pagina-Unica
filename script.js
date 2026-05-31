52a0bad96a05fa1529beaaafa2eb486f9112a9ac
kauanr <kauanrodrigues63624@gmail.com>
2026-05-31 07:09:58 -0300

    java 1% iniciado

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const btnDesafio3 = document.querySelector("btnDesafio3")
 const btnDesafio4 = document.querySelector("btnDesafio4")
 const btnDesafio5 = document.querySelector("btnDesafio5")
 const btnDesafio6 = document.querySelector("btnDesafio6")
+
+
+
+const conversor = document.querySelector("#conversor")
