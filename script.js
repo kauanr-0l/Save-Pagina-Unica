a72046af7276f1c9435398cefcd77f9e362a2343
kauanr-0l <kauanrodrigues63624@gmail.com>
2026-06-02 22:06:44 -0300

    trocando

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-const btnDesafio1 = document.querySelector("#btnDesafio1")
-const btnDesafio2 = document.querySelector("#btnDesafio2")
-const btnDesafio3 = document.querySelector("#btnDesafio3")
-const btnDesafio4 = document.querySelector("#btnDesafio4")
-const btnDesafio5 = document.querySelector("#btnDesafio5")
-const btnDesafio6 = document.querySelector("#btnDesafio6")
+const paginas = document.querySelectorAll(".pagina");
+const aside = document.querySelector("aside");
 
+aside.addEventListener("click", (e) => {
 
+    const secao = e.target.dataset.section;
 
-const conversor = document.getElementById("conversor")
-const imc = document.getElementById("imc")
-const temperatura = document.getElementById("temperatura")
-const velocidade = document.getElementById("velocidade")
-const massa = document.getElementById("massa")
-const regra = document.getElementById("regra-de-tres")
+    if (!secao) return;
 
+    paginas.forEach((pagina) => {
+        pagina.classList.remove("ativa");
+    });
 
-
-const desafios = [
-  conversor,
-  imc,
-  temperatura,
-  velocidade,
-  massa,
-  regra
-]
-
-
-btnDesafio1.addEventListener("click", () => {
-
-    conversor.classList.add("ativo")
-
-})
-
-function mostrarDesafio(desafioAtivo) {
-  desafios.forEach(item => {
-    item.classList.remove("ativo")
-  })
-
-  desafioAtivo.classList.add("ativo")
-}
+    document.getElementById(secao).classList.add("ativa");
+});
